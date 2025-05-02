50832b88f306643af08995b483474d0c61189109
Christian Kronborg Skeide <37899951+cskeide@users.noreply.github.com>
2025-05-02 11:48:11 +0200

    Allow SuiteSetup to run without an existing .suitecloud folder

--- lua/suitecloud.lua
+++ lua/suitecloud.lua
@@ -42,7 +42,20 @@ end
 
 M.setup_account = function()
 	local cmd = "suitecloud account:setup"
-	run_in_project_root(cmd)
+
+	vim.fn.jobstart(cmd, {
+		stdout_buffered = true,
+		on_stdout = function(_, data)
+			if data then
+				vim.api.nvim_echo({ { table.concat(data, "\n"), "Normal" } }, false, {})
+			end
+		end,
+		on_stderr = function(_, err_data)
+			if err_data then
+				vim.api.nvim_err_writeln(table.concat(err_data, "\n"))
+			end
+		end,
+	})
 end
 
 M.download_file = function()
