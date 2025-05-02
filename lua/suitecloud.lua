802f2cc735f49da9da5c31800e3d549b09beac9d
Christian Kronborg Skeide <37899951+cskeide@users.noreply.github.com>
2025-05-02 12:13:11 +0200

    run account setup in a split terminal

--- lua/suitecloud.lua
+++ lua/suitecloud.lua
@@ -41,21 +41,7 @@ M.upload_current_file = function()
 end
 
 M.setup_account = function()
-	local cmd = "suitecloud account:setup"
-
-	vim.fn.jobstart(cmd, {
-		stdout_buffered = true,
-		on_stdout = function(_, data)
-			if data then
-				vim.api.nvim_echo({ { table.concat(data, "\n"), "Normal" } }, false, {})
-			end
-		end,
-		on_stderr = function(_, err_data)
-			if err_data then
-				vim.api.nvim_err_writeln(table.concat(err_data, "\n"))
-			end
-		end,
-	})
+	vim.cmd("split | terminal suitecloud account:setup")
 end
 
 M.download_file = function()
